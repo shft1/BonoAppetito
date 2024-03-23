d825e8b3ae83f496b03689588d90e37e425fec4f
SHIFT <alexmal24122004@gmail.com>
2024-03-23 14:18:36 +0300

    intermediate result custom user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from recipes.models import Tags, Ingredients
+from .models import UserCustom
 
 admin.site.register(Tags)
 admin.site.register(Ingredients)
+admin.site.register(UserCustom)
