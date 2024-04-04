6c03fa564bba03c26b084b1cb800ac6171cfa24e
SHIFT <alexmal24122004@gmail.com>
2024-04-05 01:28:41 +0300

    start recipes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -87,6 +87,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.UserCustom'
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,9 @@
 from colorfield.fields import ColorField
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Ingredients(models.Model):
@@ -17,3 +21,57 @@ class Tags(models.Model):
 
     def __str__(self):
         return self.slug[:25]
+
+
+# class Favorite(models.Model):
+#     recipes = models.ForeignKey(
+
+#     )
+#     users = models.ForeignKey(
+
+#     )
+
+
+class Recipes(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    ingredients = models.ManyToManyField(
+        Ingredients, through='Recipes_Ingredients'
+    )
+    tags = models.ManyToManyField(
+        Tags, through='Recipes_Tags'
+    )
+    image = models.ImageField(
+        upload_to='recipes/images/',
+    )
+    name = models.CharField(max_length=200)
+    text = models.TextField()
+    cooking_time = models.IntegerField(validators=[MinValueValidator(1)])
+
+
+class Recipes_Ingredients(models.Model):
+    recipes = models.ForeignKey(
+        Recipes, on_delete=models.CASCADE, related_name='ingredients'
+    )
+    ingredients = models.ForeignKey(
+        Ingredients, on_delete=models.CASCADE, related_name='recipes'
+    )
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.recipes} - {self.ingredients}'
+
+
+class Recipes_Tags(models.Model):
+    recipes = models.ForeignKey(
+        Recipes, on_delete=models.CASCADE, related_name='tags'
+    )
+    tags = models.ForeignKey(
+        Tags, on_delete=models.CASCADE, related_name='recipes'
+    )
+
+    def __str__(self):
+        return f'{self.recipes} - {self.tags}'
