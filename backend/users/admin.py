967d9268a458a516dca58a47c29b1113a5eaebbb
alex <alexmal24122004@gmail.com>
2024-05-01 20:40:10 +0300

    change to PC

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from recipes.models import Ingredients, Recipes, Shopping_Cart, Tags
+from recipes.models import (Ingredients, Recipe_Favorite, Recipes,
+                            Recipes_Ingredients, Shopping_Cart, Tags)
 
 from .models import Subscription, UserCustom
 
@@ -8,9 +9,16 @@ class SearchAdmin(admin.ModelAdmin):
     search_fields = ["^name"]
 
 
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ['email', 'username']
+    search_fields = ['^name']
+
+
 admin.site.register(Tags, SearchAdmin)
 admin.site.register(Ingredients, SearchAdmin)
-admin.site.register(UserCustom, SearchAdmin)
-admin.site.register(Subscription, SearchAdmin)
 admin.site.register(Recipes, SearchAdmin)
+admin.site.register(Recipes_Ingredients, SearchAdmin)
+admin.site.register(Recipe_Favorite, SearchAdmin)
 admin.site.register(Shopping_Cart, SearchAdmin)
+admin.site.register(UserCustom, UserAdmin)
+admin.site.register(Subscription, SearchAdmin)
