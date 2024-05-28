a32bea590c0735d7ceda801caf17e63ad8dceaae
alex <alexmal24122004@gmail.com>
2024-05-28 11:52:41 +0300

    review #1 (isort)

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -4,15 +4,14 @@ from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
+from recipes.models import (Ingredients, RecipeFavorite, Recipes, ShoppingCart,
+                            Tags)
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-
-from recipes.models import (Ingredients, RecipeFavorite, Recipes, ShoppingCart,
-                            Tags)
 from users.models import Subscription
 
 from .filters import RecipeFilter
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from recipes.models import (Ingredients, RecipeFavorite, Recipes,
                             RecipesIngredients, ShoppingCart, Tags)
 
